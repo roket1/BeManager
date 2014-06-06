a7d49146302437782e6b21ccc0b71c91b44797b9
roket1 <Antonio@192.168.1.15>
2014-06-06 11:49:11 +0200

    filter

---
+++ BeLogin/src/main/java/it/begear/filters/filtroLogin.java
@@ -0,0 +1,23 @@
+package it.begear.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class filtroLogin extends GenericFilterBean{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
